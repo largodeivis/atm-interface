f2b45ddba96832c4ef9e3629c2189912ad88607c
Deivis Largo <dlargo67@gmail.com>
2022-01-07 14:20:51 -0600

    initial commit

---
+++ src/test/java/com/atm/atminterface/AtmInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atm.atminterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
